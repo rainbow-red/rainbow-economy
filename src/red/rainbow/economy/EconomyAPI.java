0aca964be1cd32f5c552a7bfea73b9b242427593
RainbowSpeedy <rs@rainbow.red>
2020-12-15 11:01:05 +0100

    Set up basic API

--- src/red/rainbow/economy/EconomyAPI.java
+++ src/red/rainbow/economy/EconomyAPI.java
@@ -1,12 +1,33 @@
 package red.rainbow.economy;
 
+import de.remadisson.rainbowcore.api.UserDataAPI;
+
 import java.util.UUID;
 
 public class EconomyAPI {
 
     private final UUID playerUUID;
+    private final UserDataAPI userData = new UserDataAPI();
+    private double cash;
+    private double coins;
+
 
     public EconomyAPI(UUID uuid) {
         this.playerUUID = uuid;
+        if (!userData.isLoaded(playerUUID)) {
+            userData.loadUser(playerUUID);
+        }
+    }
+
+    public static void initPlayerEco() {
+        //TODO
+    }
+
+    public double getCash() {
+        return this.cash;
+    }
+
+    public double getCoins() {
+        return this.coins;
     }
 }
